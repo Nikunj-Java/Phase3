5a6eb62a46528ccb557cef88c5ac14d81fe335c6
unknown <ganitniduniyanikunj10@gmail.com>
2022-04-05 17:42:44 +0530

    Session 4 notes

--- phase4/Session-4/myapp/src/app/app.module.ts
+++ phase4/Session-4/myapp/src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { BindingComponent } from './binding/binding.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    BindingComponent
   ],
   imports: [
     BrowserModule,
