3dba15da43f045d584aa5c273529473e17dfb512
unknown <ganitniduniyanikunj10@gmail.com>
2022-04-06 15:01:03 +0530

    Session 5 notes

--- phase4/Session-4/myapp/src/app/binding/binding.component.ts
+++ phase4/Session-4/myapp/src/app/binding/binding.component.ts
@@ -14,5 +14,16 @@ export class BindingComponent implements OnInit {
 
   name:string ="Nikunj Soni";
   url:string ="https://www.google.com";
+  we:number=700;
+  products=[
+    {name:"pencil",price:"8.3",available:"04-04-2022",rating:4.8},
+    {name:"pen",price:"10",available:"05-04-2022",rating:4.0},
+    {name:"eraser",price:"5",available:"03-04-2022",rating:4.5},
+    {name:"scale",price:"20",available:"10-04-2022",rating:4.9}
+  ];
 
+  flag=false;
+  show(){
+    this.flag=!this.flag;
+  }
 }
